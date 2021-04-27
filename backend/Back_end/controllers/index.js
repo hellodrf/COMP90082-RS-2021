428efbbcadd22b769e31e7d8d5c606f9ac8b4963
hellodrf <r.du1@student.unimelb.edu.au>
2021-04-27 23:36:08 +0800

    Updates

--- backend/Back_end/controllers/index.js
+++
@@ -1,8 +0,0 @@
-exports.User        = require('./user');
-exports.Subject     = require('./subject');
-exports.Comment     = require('./comment');
-exports.Tool        = require('./tool');
-exports.Aritcle     = require('./article');
-exports.Notification = require('./notification');
-exports.Section     = require('./section');
-exports.Search     = require('./search');
